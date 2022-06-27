67f5c985987e98c69d5c23595288a284dcafc488
Ken Yurino <ken.yurino@gmail.com>
2022-06-27 18:22:42 +0900

    Initial Commit

---
+++ CodeReader/CodeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeReaderApp.swift
+//  CodeReader
+//
+//  Created by Ken Yurino on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct CodeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
